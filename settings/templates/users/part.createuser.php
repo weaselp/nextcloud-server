510d9c4705d7fcfdd5764d57246cf14f1589b49d
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-01-08 05:37:35 +0100

    New menu NOT in popover
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- settings/templates/users/part.createuser.php
+++ settings/templates/users/part.createuser.php
@@ -1,52 +1,37 @@
 <div class="app-navigation-new">
 	<button type="button" class="icon-add"><?php p($l->t('New user'))?></button>
-	<form class="newUserMenu popovermenu menu-left" id="newuser" autocomplete="off">
-		<ul>
-			<li>
-				<label class="icon-user" for="newusername">
-					<input id="newusername" type="text" required
-						placeholder="<?php p($l->t('Username'))?>"
-						autocomplete="off" autocapitalize="none" autocorrect="off" />
-				</label>
-			</li>
-			<li>
-				<label class="icon-password" for="newuserpassword">
-					<input id="newuserpassword" type="password" required
-						   placeholder="<?php p($l->t('Password'))?>"
-						   autocomplete="off" autocapitalize="none" autocorrect="off" />
-				</label>
-			</li>
-			<li>
-				<label class="icon-mail" for="newemail">
-					<input id="newemail" type="text" style="display:none"
-						   placeholder="<?php p($l->t('E-Mail'))?>"
-						   autocomplete="off" autocapitalize="none" autocorrect="off" />
-				</label>
-			</li>
-			<li>
-				<label class="icon-contacts-dark groups" for="newgroup">
-					<div class="groupsListContainer multiselect button" data-placeholder="<?php p($l->t('Groups'))?>"><span class="title groupsList"></span>
-						<span class="icon-triangle-s"></span>
-					</div>
-				</label>
-			</li>
-			<li>
-				<label class="icon-confirm" for="newsubmit">
-					<input type="submit" id="newsubmit" class="button hidden" value="" />
-					<span>
-						<?php p($l->t('Create'))?>
-					</span>
-				</label>
-			</li>
-			<?php if((bool)$_['recoveryAdminEnabled']): ?>
-			<li class="recoveryPassword">
-				<input id="recoveryPassword"
-					   type="password"
-					   placeholder="<?php p($l->t('Admin Recovery Password'))?>"
-					   title="<?php p($l->t('Enter the recovery password in order to recover the users files during password change'))?>"
-					   alt="<?php p($l->t('Enter the recovery password in order to recover the users files during password change'))?>"/>
-			</li>
-			<?php endif; ?>
-		</ul>
+	<form class="newUserMenu" id="newuser" autocomplete="off">
+		<div>
+			<input id="newusername" type="text" required
+				placeholder="<?php p($l->t('Username'))?>" name="username"
+				autocomplete="off" autocapitalize="none" autocorrect="off" />
+		</div>
+		<div>
+			<input id="newuserpassword" type="password" required
+				   placeholder="<?php p($l->t('Password'))?>" name="password"
+				   autocomplete="off" autocapitalize="none" autocorrect="off" />
+		</div>
+		<div>
+			<input id="newemail" type="text" style="display:none"
+				   placeholder="<?php p($l->t('E-Mail'))?>" name="email"
+				   autocomplete="off" autocapitalize="none" autocorrect="off" />
+		</div>
+		<div>
+			<label class="groups" for="newgroup">
+				<div class="groupsListContainer multiselect button" data-placeholder="<?php p($l->t('Groups'))?>"><span class="title groupsList"></span>
+					<span class="icon-triangle-s"></span>
+				</div>
+			</label>
+			<input type="submit" id="newsubmit" class="button icon-confirm has-tooltip" value="" title="<?php p($l->t('Create'))?>" />
+		</div>
+		<?php if((bool)$_['recoveryAdminEnabled']): ?>
+		<div class="recoveryPassword">
+			<input id="recoveryPassword"
+				   type="password"
+				   placeholder="<?php p($l->t('Admin Recovery Password'))?>"
+				   title="<?php p($l->t('Enter the recovery password in order to recover the users files during password change'))?>"
+				   alt="<?php p($l->t('Enter the recovery password in order to recover the users files during password change'))?>"/>
+		</div>
+		<?php endif; ?>
 	</form>
 </div>
